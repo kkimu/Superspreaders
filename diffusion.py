ae379434f0b4ceca67c90c58182353c167f99547
kkimu <i08316.love@gmail.com>
2015-09-05 19:34:20 +0900

    Clean

--- diffusion.py
+++ diffusion.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 
 t0 = time.time() 
 
-data = "Twitter"  #Twitter Facebook APS Twitter_NoW
+dataset = "Twitter"  #Twitter Facebook APS Twitter_NoW
 
 diffusion = defaultdict(set)
-for line in open("{}/data/diffusion.txt".format(data)):
+for line in open("{}/data/diffusion.txt".format(dataset)):
 #for line in oepn("data/diffusion.txt"):
     sp = line.strip().split(" ")
     diffusion[sp[1]].add(sp[0])
     
-f = open("{}/data/diffusion_result.txt".format(data),"w")
+f = open("{}/data/diffusion_result.txt".format(dataset),"w")
 for k,v in diffusion.items():
     diffusion[k] = len(v)
 for k,v in sorted(diffusion.items(),key=lambda x:x[1],reverse=True):
